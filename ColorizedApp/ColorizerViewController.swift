b2ec393d9c66c7857dd62c94ee2fe122c13362a8
Igor Guryan <sacujo93@yandex.ru>
2024-09-16 01:14:25 +0300

    initial and final commit

---
+++ ColorizedApp/ColorizerViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Igor Guryan on 15.09.2024.
+//
+
+import UIKit
+
+class ColorizerViewController: UIViewController {
+
+    @IBOutlet weak var colorizedView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    var redColorValue: CGFloat = 0
+    var greenColorValue: CGFloat = 0
+    var blueColorValue: CGFloat = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        colorizedView.layer.cornerRadius = view.frame.width / 15
+        colorizedView.backgroundColor = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
+    }
+
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        switch sender {
+        case redSlider:
+            redValueLabel.text = String(format: "%.2f", redSlider.value)
+            redColorValue = CGFloat(redSlider.value)
+        case greenSlider:
+            greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+            greenColorValue = CGFloat(greenSlider.value)
+        default:
+            blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+            blueColorValue = CGFloat(blueSlider.value)
+        }
+        
+        colorizedView.backgroundColor = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
+    }
+    
+}
+
